0884089e869cd6c4cb7756eff17dd07f2b6a8a23
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-27 23:52:58 +0500

    Update postads.js

--- server/models/postads.js
+++ server/models/postads.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
     title: String,
-    message: String,
+    description: String,
     name: String,
     creator: String,
     tags: [String],
