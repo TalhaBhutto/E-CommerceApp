e2de25834ff99dee792e1aed2452af05a294cb6b
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-05-09 16:05:01 +0500

    added contact option

--- server/models/postads.js
+++ server/models/postads.js
@@ -17,6 +17,10 @@ const postSchema = mongoose.Schema({
         type:String,
         required:true
     },
+    contact:{
+        type:Number,
+        required:true
+    },
     price:{
         type:Number,
         required:true
